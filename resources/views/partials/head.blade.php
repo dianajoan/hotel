94b591c72ee9f929a2bf3e44a53e657b8344589b
dianajoan <dianajoan900@gmail.com>
2019-08-27 12:34:16 +0300

    Added a background image in the links of the pages

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -24,13 +24,13 @@
 <![endif]-->
 
 <link href="{{ url('adminlte/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-<link rel="stylesheet"
-      href="{{ url('quickadmin/css') }}/select2.min.css"/>
+<link rel="stylesheet" href="{{ url('quickadmin/css') }}/select2.min.css"/>
 <link href="{{ url('adminlte/css/AdminLTE.min.css') }}" rel="stylesheet">
+<link href="{{ url('adminlte/css/AdminLTE.css') }}" rel="stylesheet">
 <link href="{{ url('adminlte/css/custom.css') }}" rel="stylesheet">
-<link href="{{ url('adminlte/css/skins/skin-blue.min.css') }}" rel="stylesheet">
+<link href="{{ url('adminlte/css/skins/skin-purple.min.css') }}" rel="stylesheet">
 <!-- Favicon -->
-<link rel="shortcut icon" href="{!! asset('images/favicon.ico') !!}">
+<link rel="shortcut icon" href="{!! asset('favicon.ico') !!}">
 <link rel="stylesheet"
       href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
 <link rel="stylesheet"
